3420f99c2cb87e644f8eb5508e8e79aaaad4c915
Bayu Prakoso <bayuprakoso.adi@gmail.com>
2020-09-21 13:25:51 +0700

    my-portfolio init

---
+++ colors.js
@@ -0,0 +1,14 @@
+module.exports = {
+  background: '#F4F0BB',
+  backgroundDark: '#87C38F',
+
+  text: '#DA2C38',
+
+  primary: '#DA2C38',
+  primaryLight: '#43291F',
+  primaryDark: '#43291F',
+
+  secondary: '#DA2C38',
+  secondaryLight: '#87C38F',
+  secondaryDark: '#43291F',
+};
